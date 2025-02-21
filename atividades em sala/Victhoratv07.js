93f0ec0d1ac733b84a90714014ac31e0adaaddb8
Victhorambrosio <mayaraevicthor@gmail.com>
2025-02-21 10:28:41 -0300

    feito com mano gusta professor

--- atividades em sala/Victhoratv07.js
+++ atividades em sala/Victhoratv07.js
@@ -8,24 +8,43 @@ Dada a matriz abaixo, faça um programa em JavaScript que exiba ao usuário os e
 |  1  9  2  |
 |  7  1  4  |
 */
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+for (let linha = 0; linha < matriz.length; linha++) {
+  for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+    linha == coluna &&
+      console.log(`Matriz[${linha}][${coluna}] = ${matriz[linha][coluna]}`);
+  }
+}
 
+/*Dada a matriz abaixo, faça um programa em JavaScrpit que multiplique seus elementos por um valor x e exiba ao usuário a matriz antes e após a multiplicação.
+|  3   5   8   |
+|  1   9   2   |
+|  7   1   4   |
+
+*/
 var matriz = [
   [3, 5, 8],
   [1, 9, 2],
   [7, 1, 4],
 ];
 
-console.log("-------------------------------");
-
+var x = 4;
 console.table(matriz);
 
 for (let linha = 0; linha < matriz.length; linha++) {
   for (let coluna = 0; coluna < matriz[0].length; coluna++) {
-    console.log(
-      `Matriz [${linha[0][1][2]}][${coluna[0][1][2]}] = ${
-        matriz[linha[0][1][2]][coluna[0][1][2]]
-      }
-        }`
-    );
+    matriz[linha][coluna] *= x;
   }
+}
+console.table(matriz);
+
+
+//Faça um loop FOR onde apresente os números ímpares de 0 a 100;
+//Não pode ser resolvido com conta ou resto de divisão
+for (let i = 0; i <= 100; i++) {
+  console.log(i);
 }
